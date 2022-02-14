01c74931318dba1dc34b61635f6459254f7f5906
xmasuhai <xmasuhai@163.com>
2022-02-14 11:42:17 +0800

    🎨 完成首页结构和样式

--- src/pages/blog/index/BlogIndex.vue
+++ src/pages/blog/index/BlogIndex.vue
@@ -1,21 +1,112 @@
 <script setup lang="ts" name="BlogIndex">
+/*
+
 import {Button, message} from 'ant-design-vue';
 import {inject} from 'vue';
 
 const popMessage = inject<typeof message>('$message');
+*/
+
 </script>
 
 <template>
-  <Button class="tempBtn" @click="popMessage.error('这是一条错误消息', 2)">
-    博客首页
-  </Button>
+  <section class="blog-post">
+
+    <div class="item">
+      <figure class="avatar">
+        <img src="" alt="">
+        <figcaption>
+          姓名
+        </figcaption>
+      </figure>
+      <h3 class="title">
+        文章标题
+        <span class="date">日期</span>
+      </h3>
+      <p class="article">
+        正文，最多显示前200字
+      </p>
+    </div>
+    <div class="item">
+      <figure class="avatar">
+        <img src="" alt="">
+        <figcaption>
+          姓名
+        </figcaption>
+      </figure>
+      <h3 class="title">
+        文章标题
+        <span class="date">日期</span>
+      </h3>
+      <p class="article">
+        正文，最多显示前200字
+      </p>
+    </div>
+    <div class="item">
+      <figure class="avatar">
+        <img src="" alt="">
+        <figcaption>
+          姓名
+        </figcaption>
+      </figure>
+      <h3 class="title">
+        文章标题
+        <span class="date">日期</span>
+      </h3>
+      <p class="article">
+        正文，最多显示前200字
+      </p>
+    </div>
+
+  </section>
 </template>
 
 <style lang="scss" scoped>
-@import '../../../assets/style/main.scss';
+@import 'src/assets/style/variables';
 
-.tempBtn {
-  @extend %blog-btn;
+.blog-post {
+  .item {
+    display: grid;
+    grid: auto auto / 80px 1fr;
+    margin: 20px 0;
+
+    .avatar {
+      grid-column: 1;
+      grid-row: 1 / span 2;
+      justify-self: center;
+      margin-left: 0;
+      text-align: center;
+
+      img {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+      }
+
+      figcaption {
+        font-size: 12px;
+        color: $theme-lighter-color;
+      }
+    }
+
+    .title {
+      grid-column: 2;
+      grid-row: 1;
+
+      & > .date {
+        color: $theme-lighter-color;
+        font-size: 12px;
+        font-weight: normal;
+      }
+    }
+
+    .article {
+      grid-column: 2;
+      grid-row: 2;
+      margin-top: 0;
+    }
+
+  }
 }
 
 </style>
