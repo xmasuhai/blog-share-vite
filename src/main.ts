c52caf97ba3860dc5eaaa45916d492b1a76542bc
xmasuhai <xmasuhai@163.com>
2022-02-18 06:10:54 +0800

    :wrench: 使用 vuex 管理状态，并添加 TypeScript 支持

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import App from '@/App.vue';
 import {createApp} from 'vue';
-import App from './App.vue';
-import router from './router/index';
+import {store, key} from '@/store';
+import router from '@/router';
 
 const app = createApp(App);
+app.use(store, key);
 app.use(router);
 app.mount('#app');
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import {createStore, Store} from 'vuex';
+import {InjectionKey} from 'vue';
 
-// Vue.use(Vuex)
+// 为 store state 声明类型
+export interface State {
+  count: number;
+}
+
+// 定义 injection key
+export const key: InjectionKey<Store<State>> = Symbol();
+
+// 创建一个新的 store 实例
+export const store = createStore<State>({
+  state() {
+    return {
+      count: 0
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+});
